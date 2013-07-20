b199516d0299fb88c9637d4136b7d2e7d322e45c
Bram Schoenmakers <me@bramschoenmakers.nl>
2013-07-20 10:43:39 +0200

    Add filter for relevance.
    A todo is considered relevant iff:
    
    * It has not been completed;
    * There is no start date, or the start date is today or in the past;
    * Or one of the following conditions:
      * It has top priority.
      * It has priority B and is due within 30 days.
      * It has priority C or lower and is due within 14 days.

---
+++ filter-relevance.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+
+use TodoTxt;
+
+# A todo item is relevant iff:
+#
+# The item has not been completed, AND
+#
+# The start date is blank, today or in the past, AND one of the following conditions:
+#
+# * The priority is (A).
+#
+# * The priority is (B) and due date is within 30 days.
+#
+# * The priority is (C) or lower and due date is within 14 days.
+#
+# * There is no due date
+
+my $todos = TodoTxt::getTodos();
+foreach my $todo ( values %$todos ) {
+  next if $todo->{ 'completed' };
+  next if !TodoTxt::isActive( $todo );
+
+  next if $todo->{ 'priority' } eq 'B'
+       && TodoTxt::hasDueDate( $todo )
+       && TodoTxt::getDaysLeft( $todo ) > 30;
+
+  next if ( $todo->{ 'priority' } ne 'A' && $todo->{ 'priority' } ne 'B' )
+       && TodoTxt::hasDueDate( $todo )
+       && TodoTxt::getDaysLeft( $todo ) > 14;
+
+  print $todo->{ 'src' };
+}
+
