dde90f2086b088ad9af34d270fada10b1cd3a0e9
Bram Schoenmakers <me@bramschoenmakers.nl>
2013-07-29 21:52:42 +0200

    Create new filter which hides todos which has uncompleted dependencies.
    An additional constraint is that the todo should not be part of a cycle,
    in that case a todo should also be shown. However, this condition is not
    implemented yet.
    
    Example:
    
    2013-07-29 Task 1       id:1
    2013-07-29 Task 1.1     dep:1 id:2
    2013-07-29 Task 1.2     dep:1
    2013-07-29 Task 1.1.1   dep:2
    
    The result after applying this filter will be:
    
    2013-07-29 Task 1.2     dep:1
    2013-07-29 Task 1.1.1   dep:2

---
+++ filter-dependencies.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+# TodoTxt Tools
+# Copyright (C) 2013 Bram Schoenmakers <me@bramschoenmakers.nl>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+
+use Depends;
+use TodoTxt;
+
+sub inCycle {
+  return 0;
+}
+
+sub hasUnfinishedDependencies {
+  my $todo = $_[ 0 ];
+
+  # check if this todo can be referred to at all
+  return 0 unless defined( $todo->{ 'tags' }->{ 'id' } );
+
+  my @dependencies = TodoTxt::tasksReferringToId( $todo->{ 'tags' }->{ 'id' }->[ 0 ] );
+  return grep { !defined( $_->{ 'completed' } ) || $_->{ 'completed' } == 0 } @dependencies;
+}
+
+# print if todo:
+# * appears in a cycle
+# * there are no unfinished todos on which it depends
+
+my $todos = TodoTxt::readTodos( $ARGV[ 0 ] );
+foreach my $todo ( @$todos ) {
+  next if hasUnfinishedDependencies( $todo ) && !inCycle( $todo );
+
+  print $todo->{ 'src' };
+}
