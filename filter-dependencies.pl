f41ba03cb6180204fd87cffe5abd7af93264eb5a
Bram Schoenmakers <me@bramschoenmakers.nl>
2013-08-01 22:29:37 +0200

    Remove cycle detection code in filter-dependencies.pl
    Just trigger on the fact that a task has unfinished (direct)
    dependencies.

--- filter-dependencies.pl
+++ filter-dependencies.pl
@@ -21,40 +21,13 @@ use strict;
 use Depends;
 use TodoTxt;
 
-sub inCycle {
-  my $root = $_[ 0 ];
-
-  my %visited;
-  $visited{ $root->{ 'src' } } = 1;
-
-  my @queue = TodoTxt::getDependencies( $root );
-  while ( @queue ) {
-    my $todo = shift @queue;
-
-    return 1 if $todo->{ 'src' } eq $root->{ 'src' };
-    next if defined( $visited{ $todo->{ 'src' } } );
-
-    $visited{ $todo->{ 'src' } } = 1;
-
-    @queue = ( @queue, TodoTxt::getDependencies( $todo ) );
-  }
-
-  return 0;
-}
-
 sub hasUnfinishedDependencies {
   my $todo = $_[ 0 ];
 
-  return grep { !defined( $_->{ 'completed' } ) || $_->{ 'completed' } == 0 } getDependencies( $todo );
+  return grep { !defined( $_->{ 'completed' } ) || $_->{ 'completed' } == 0 } TodoTxt::getDirectDependencies( $todo );
 }
-
-# print if todo:
-# * appears in a cycle
-# * there are no unfinished todos on which it depends
 
 my $todos = TodoTxt::readTodos( $ARGV[ 0 ] );
 foreach my $todo ( @$todos ) {
-  next if hasUnfinishedDependencies( $todo ) && !inCycle( $todo );
-
-  print $todo->{ 'src' };
+  print $todo->{ 'src' } unless hasUnfinishedDependencies( $todo );
 }
