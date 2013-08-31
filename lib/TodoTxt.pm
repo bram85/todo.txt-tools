92ea971dabedb974d78934aa27324c3a46e06abe
Bram Schoenmakers <me@bramschoenmakers.nl>
2013-08-31 23:29:27 +0200

    Extend relative dates with the weekdays.
    Use "sat" or "saturday" to indicate next saturday (or today if today is
    saturday).

--- lib/TodoTxt.pm
+++ lib/TodoTxt.pm
@@ -293,22 +293,54 @@ sub writeTodos {
   close( $fh ) if defined( $filename );;
 }
 
+sub convertPattern {
+  my $pattern = $_[ 0 ];
+  my $offset = $_[ 1 ];
+
+  my ( $amount, $period ) = ( $1, $2 );
+
+  $offset += $amount * DAY               if $period eq 'd';
+  $offset += $amount * WEEK              if $period eq 'w';
+  $offset = $offset->add_months( $amount ) if $period eq 'm';
+  $offset = $offset->add_years( $amount )  if $period eq 'y';
+
+  return $offset;
+}
+
+sub convertWordPattern {
+  my $pattern = $_[ 0 ];
+  my $offset = localtime();
+
+  return convertPattern( "0d", $offset ) if $pattern eq "today";
+  return convertPattern( "1d", $offset ) if $pattern eq "tomorrow";
+}
+
+sub convertWeekdayPattern {
+  my $targetDay = $_[ 0 ];
+  my $offset = localtime();
+
+  my $day = $offset->day;
+  while ( $targetDay !~ /$day/i ) {
+    $offset += DAY;
+    $day = $offset->day;
+  }
+
+  return $offset;
+}
+
 # input/output: Time::Piece
 sub convertRelativeDate {
   my $pattern = $_[ 0 ];
   my $offset = defined( $_[ 1 ] ) ? $_[ 1 ] : localtime();
 
-  if ( $pattern =~ /^(\d+)([dwmy])$/ ) {
-    my ( $amount, $period ) = ( $1, $2 );
-
-    $offset += $amount * DAY               if $period eq 'd';
-    $offset += $amount * WEEK              if $period eq 'w';
-    $offset = $offset->add_months( $amount ) if $period eq 'm';
-    $offset = $offset->add_years( $amount )  if $period eq 'y';
+  if ( $pattern =~ /^(\d+)([dwmy])$/i ) {
+    $offset = convertPattern( $pattern, $offset );
+  }
+  elsif ( $pattern =~ /^(today|tomorrow)$/i ) {
+    $offset = convertWordPattern( $pattern );
   }
   else {
-    $offset = localtime();
-    $offset += DAY if $pattern eq 'tomorrow';
+    $offset = convertWeekdayPattern( $pattern );
   }
 
   return $offset;
@@ -325,8 +357,8 @@ sub convertRelativeDateString {
   return $date->ymd;
 }
 
-sub isRecurrencePattern {
-  return $_[ 0 ] =~ /^\d+[dwmy]|today|tomorrow$/;
+sub isRelativeDatePattern {
+  return $_[ 0 ] =~ /^(\d+[dwmy]|today|tomorrow|mon(day)?|tue(sday)?|wed(nesday)?|thu(rsday)?|fri(day)?|sat(day)?|sun(day)?)$/i;
 }
 
 sub printTodo {
